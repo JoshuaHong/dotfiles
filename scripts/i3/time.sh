2bd9ba40a5001ce51de1dc80f8afffaeaa722b4b
Joshua Hong <j99hong@gmail.com>
2019-11-18 22:59:57 -0500

    Updated time script to close on click

--- scripts/i3/time.sh
+++ scripts/i3/time.sh
@@ -6,7 +6,9 @@
 # Notifications
 # Uses the same parameters as the dunstify command
 notify() {
-  dunstify -t 1200 -h string:x-canonical-private-synchronous:"time" "$@"
+  if [ $(dunstify -t 980 -h string:x-canonical-private-synchronous:"time" -A Y,yes "$@") -eq 2 ]; then
+    pkill -KILL time.sh
+  fi
 }
 
 # Full text
@@ -17,15 +19,15 @@ date "+%R"
 
 case $BLOCK_BUTTON in
   1) # Left click
-    for i in {1..3}; do
+    end=$((SECONDS+3))
+    while [ $SECONDS -lt $end ]; do
       notify "Time" "🕛 $(date +%T)"
-      sleep 1
     done
     ;;
   3) # Right click
-    for i in {1..60}; do
+    end=$((SECONDS+60))
+    while [ $SECONDS -lt $end ]; do
       notify "Time" "🕛 $(date +%T)"
-      sleep 1
     done
     ;;
 esac
