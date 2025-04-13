e77640a91f739d54c261df98c906b1a37b931a8f
Joshua Hong <hong.josh@protonmail.com>
2025-04-13 02:45:50 -0700

    Retry on weather curl failure

--- .config/yambar/scripts/weather.sh
+++ .config/yambar/scripts/weather.sh
@@ -4,11 +4,25 @@
 
 LOCATION="Sunnyvale"
 
-weather=($(curl --fail --silent "v2d.wttr.in/${LOCATION}?m" | grep "Weather:"))
-icon="${weather[1]}"
-temperature="${weather[-4]}"
-temperature="${temperature//","/""}"  # Replace "," with "".
-temperature="${temperature//"+"/""}"  # Replace "+" with "".
-temperature="${temperature//"-0"/"0"}"  # Replace "-0" with "0".
-echo "weather|string|${icon} ${temperature}"
-echo ""
+main() {
+    weather="$(fetchWeather)"
+    while [[ -z ${weather} ]]; do
+        sleep 10
+        weather="$(fetchWeather)"
+    done
+
+    weather=($(echo "${weather}" | grep "Weather:"))
+    icon="${weather[1]}"
+    temperature="${weather[-4]}"
+    temperature="${temperature//","/""}"  # Replace "," with "".
+    temperature="${temperature//"+"/""}"  # Replace "+" with "".
+    temperature="${temperature//"-0"/"0"}"  # Replace "-0" with "0".
+    echo "weather|string|${icon} ${temperature}"
+    echo ""
+}
+
+fetchWeather() {
+    curl --fail --silent "v2d.wttr.in/${LOCATION}?m"
+}
+
+main
