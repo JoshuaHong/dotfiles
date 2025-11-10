5c95432008b59e55f46c833d29f9fe2707126339
Joshua Hong <hong.josh@protonmail.com>
2025-11-10 12:23:54 -0800

    Fix bug where user cannot enter password to unlock gpg password

--- .local/src/password.sh
+++ .local/src/password.sh
@@ -9,11 +9,30 @@
 #     password
 
 main() {
-    local -a passwords=( "${PASSWORD_STORE_DIR}"**/*.gpg )
-    passwords=( "${passwords[@]#"${PASSWORD_STORE_DIR}"/}" )
-    passwords=( "${passwords[@]%.gpg}" )
-    local -r password="$(printf '%s\n' "${passwords[@]}" | fuzzel --dmenu "${@}")"
-    pass show --clip "${password}"
+    local -r password="$(selectPassword)"
+
+    if $(hasCachedPassword); then
+        pass show --clip "${password}"
+    else
+        foot pass show --clip "${password}"
+    fi
+}
+
+selectPassword() {
+    local -a passwords
+    parsePasswords passwords
+    printf '%s\n' "${passwords[@]}" | fuzzel --dmenu "${@}"
+}
+
+parsePasswords() {
+    local -n passwordFiles="${1}"
+    passwordFiles=( "${PASSWORD_STORE_DIR}"**/*.gpg )
+    passwordFiles=( "${passwords[@]#"${PASSWORD_STORE_DIR}"/}" )
+    passwordFiles=( "${passwords[@]%.gpg}" )
+}
+
+hasCachedPassword() {
+    echo "test" | gpg --pinentry-mode error --sign -o /dev/null
 }
 
 main "${@}"
