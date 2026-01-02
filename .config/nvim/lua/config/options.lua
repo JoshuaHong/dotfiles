c16fa3a14e5af4de5d40b083e6ca0124e6b97ecd
Joshua Hong <hong.josh@protonmail.com>
2026-01-01 23:11:35 -0800

    Add showbreak option

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,6 +6,10 @@ vim.diagnostic.config({ virtual_text = true })
 vim.g.mapleader = " "
 -- Set the local leader key.
 vim.g.maplocalleader = " "
+-- Display the break indent.
+vim.opt.breakindent = true
+-- Set the break indent options.
+vim.opt.breakindentopt = { "shift:8", "sbr" }
 -- Use the system clipboard by default.
 vim.opt.clipboard = "unnamedplus"
 -- Set the column to be highlighted.
@@ -22,6 +26,8 @@ vim.opt.listchars = "nbsp:˽,tab:>-,trail:·"
 vim.opt.number = true
 -- Set the number of spaces per tab.
 vim.opt.shiftwidth = 4
+-- Set the break indent character.
+vim.opt.showbreak = ">>"
 -- Use case-sensitive search for capital letters.
 vim.opt.smartcase = true
 -- Set the number of columns a tab character uses.
