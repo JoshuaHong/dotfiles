16bb2373798e8203a2e16cbe634bc222d1ce6d17
Joshua Hong <hong.josh@protonmail.com>
2026-01-08 12:39:50 -0800

    Close telescope on escape immediately without pressing it twice

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,12 @@
 -- The telescope configuration file.
 
+local actions = require("telescope.actions")
+
 require("telescope").setup({
     defaults = {
         mappings = {
             i = {
+                ["<Esc>"] = actions.close,
                 ["<C-k>"] = "move_selection_next",
                 ["<C-l>"] = "move_selection_previous"
             }
