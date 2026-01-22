f0fd3c9c06d282d3eae1bda5a6a8d6c52319c9a5
Joshua Hong <hong.josh@protonmail.com>
2026-01-21 23:27:58 -0800

    Specify the brightness device to enable controlling brightness in hybrid mode with multiple devices

--- .local/src/keybindings/brightness.sh
+++ .local/src/keybindings/brightness.sh
@@ -6,40 +6,70 @@
 #     ${XDG_CONFIG_HOME}/niri/config.kdl
 #
 # Usage:
-#     brightness [up | down]
+#     brightness [up | down | save | restore | set value]
 #
 # Arguments:
-#     up     - Raise the brightness.
-#     down   - Lower the brightness.
+#     up      - Raise the brightness by INCREMENT.
+#     down    - Lower the brightness by INCREMENT.
+#     save    - Save the brightness temporarily.
+#     restore - Restore the saved brightness.
+#     set     - Set the brightness to value.
 
+declare -gr DEVICE="amdgpu_bl1"
+declare -gr INCREMENT="5%"
 declare -gr ICONS_DIRECTORY="${XDG_DATA_HOME}/assets/icons"
 
 main() {
     local -r action="${1}"
+    local -r value="${2}"
 
     if [[ "${action}" == "up" ]]; then
         raise
     elif [[ "${action}" == "down" ]]; then
         lower
+    elif [[ "${action}" == "save" ]]; then
+        save
+    elif [[ "${action}" == "restore" ]]; then
+        restore
+    elif [[ "${action}" == "set" ]]; then
+        set "${value}"
     else
         echoError "Error: Invalid operation."
         exit 1
     fi
-
-    notify
 }
 
 raise() {
-    brightnessctl --class=backlight set +5%
+    brightnessctl --class="backlight" --device="${DEVICE}" set "+${INCREMENT}"
+    notify
 }
 
 lower() {
-    brightnessctl --class=backlight set 5%-
+    brightnessctl --class="backlight" --device="${DEVICE}" set "${INCREMENT}-"
+    notify
+}
+
+set() {
+    local -r value="${1}"
+    if ! isVariableSet "${value}"; then
+        echoError "Error: Invalid brightness value."
+        exit 1
+    fi
+
+    brightnessctl --class="backlight" --device="${DEVICE}" set "${value}"
+}
+
+save() {
+    brightnessctl --class="backlight" --device="${DEVICE}" --save
+}
+
+restore() {
+    brightnessctl --class="backlight" --device="${DEVICE}" --restore
 }
 
 notify() {
-    local -r brightness="$(brightnessctl get)"
-    local -r maxBrightness="$(brightnessctl max)"
+    local -r brightness="$(brightnessctl --device="${DEVICE}" get)"
+    local -r maxBrightness="$(brightnessctl --device="${DEVICE}" max)"
     local -r value="$(( "${brightness}" * 100 / "${maxBrightness}" ))"
 
     notify-send --hint="string:x-canonical-private-synchronous:brightness" \
@@ -48,6 +78,10 @@ notify() {
             "Brightness" "${value}%"
 }
 
+isVariableSet() {
+    local -r variable="${1}"
+    [[ -n "${variable}" ]]
+}
 
 echoError() {
     local -r errorMessage="${1}"
