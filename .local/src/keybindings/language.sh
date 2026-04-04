6f4a999cda00b7e9099bed265079f1b8adf8a19d
Joshua Hong <hong.josh@protonmail.com>
2026-04-03 17:36:57 -0700

    Remove the language waybar module

--- .local/src/keybindings/language.sh
+++ .local/src/keybindings/language.sh
@@ -4,14 +4,12 @@
 #
 # References:
 #     ${XDG_CONFIG_HOME}/niri/config.kdl
-#     ${XDG_CONFIG_HOME}/waybar/config.jsonc
 #
 # Usage:
 #     language
 
 main() {
     fcitx5-remote -t
-    niri msg action switch-layout next
 }
 
 main "${@}"
