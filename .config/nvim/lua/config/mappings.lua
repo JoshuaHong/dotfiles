18de342f080c0a11b99d051aa86a6cbd24f4c09e
Joshua Hong <hong.josh@protonmail.com>
2026-06-29 18:38:45 -0400

    Swap tree keybindings for toggle and root

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -95,8 +95,8 @@ vim.keymap.set("n", "<C-o>", gitsigns.diffthis)
 vim.keymap.set("n", "<C-p>", gitsigns.blame)
 
 -- Tree key mappings.
-vim.keymap.set("n", "<C-t>", tree.change_root_to_node)
-vim.keymap.set("n", "<C-y>", function()
+vim.keymap.set("n", "<C-y>", tree.change_root_to_node)
+vim.keymap.set("n", "<C-t>", function()
     tree.toggle { focus = false }
 end)
 
